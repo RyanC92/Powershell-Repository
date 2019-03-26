5a0127e907394b00c52e2e80e614030cd0971b90
NightWolf92 <rcurran92@gmail.com>
2019-03-26 15:20:26 -0400

    WindowsPowershell default

---
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,23 @@
+Function Connect-ExOnline{
+
+$Credential = Get-Credential -Credential Rcurran@excelsiormedical.com
+
+Write-Output "Getting Exchange Online cmdlets"
+
+$session = New-PSSession -ConnectionUri https://ps.outlook.com/Powershell `
+    -ConfigurationName Microsoft.Exchange -Credential $Credential `
+    -Authentication Basic -AllowRedirection
+Import-PSSession $session
+
+Connect-MsolService -Credential $Credential
+
+}
+
+Function PWchange{
+
+$User = Read-Host "User Email Address:"
+$Password = Read-Host "Enter New Password"
+
+Set-Msoluserpassword -UserPrincipalName $User -NewPassword $Password -ForceChangePassword $False 
+
+}
