fbfaa37bd74ec41e25048873c6e54a32c3d679c1
NightWolf92 <rcurran92@gmail.com>
2019-03-26 15:17:33 -0400

    Update Test-Menu.ps1

--- Test-Menu.ps1
+++ Test-Menu.ps1
@@ -12,3 +12,25 @@ function Show-Menu
      Write-Host "3: Press '3' for this option." 
      Write-Host "Q: Press 'Q' to quit." 
 }
+
+do
+{
+    Show-Menu
+    $input = Read-host "Please Make a Selection"
+    switch($Input){
+        '1'{
+        cls
+        'You Chose Option #1'
+        }'2'{
+        cls
+        'You Chose Option #2'
+        }'3'{
+        cls
+        'You Chose Option #3'
+        }'q'{
+            return
+        }
+
+    }
+    pause
+}
