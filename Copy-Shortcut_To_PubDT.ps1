2aeec47661f2911135e4bba0c5861b14f28ff813
NightWolf92 <rcurran92@gmail.com>
2019-05-08 16:46:56 -0400

    Update Copy-Shortcut_To_PubDT.ps1
      $OpenFileDialog.Dereferencelinks = $False
    
    Allows $Shortcut to read the path of the shortcut rather than the URL inside of the shortcut.

--- Copy-Shortcut_To_PubDT.ps1
+++ Copy-Shortcut_To_PubDT.ps1
@@ -5,6 +5,7 @@ Function Get-FileName($InitialDirectory)
     [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
 
   $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+  $OpenFileDialog.Dereferencelinks = $False
   $OpenFileDialog.initialDirectory = $initialDirectory
   #$OpenFileDialog.filter = "CSV (*.csv) | *.csv"
   $OpenFileDialog.ShowDialog() | Out-Null
@@ -23,7 +24,7 @@ Function Get-FolderName($InitialDirectory)
 
 
 Write-Host "Select the Shortcut to Transfer"
-$Shortcut = Get-FolderName
+$Shortcut = Get-FileName
 
 Write-host "Select the List of Computers to transfer the shortcut to"
 $Comps = Get-FileName
@@ -31,9 +32,10 @@ $Comps = Get-FileName
 $SC = $Shortcut
 $PCS = Import-CSV $Comps
 
+$SC
 
 ForEach ($PC in $PCS){
 
+  xcopy $SC "\\$($PC.Hostname)\C$\Users\Public\Desktop"
 
-  xcopy $SC \\$PC\C$\Users\Public\Desktop
 }
