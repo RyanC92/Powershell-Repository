b4ba4ae35aa4456d5cea470b6faea454414c2e7a
NightWolf92 <rcurran92@gmail.com>
2019-05-09 11:56:32 -0400

    Update Copy-Shortcut_To_PubDT_2.ps1

--- Copy-Shortcut_To_PubDT_2.ps1
+++ Copy-Shortcut_To_PubDT_2.ps1
@@ -1,10 +1,8 @@
-$shortcut = "\\usnjfs001\shared\Ryan Curran\Scripts_Icons\medline icons\Machine Breakdown Log.website"
-
 $Comps = Import-csv "\\usnjfs001\shared\Ryan Curran\Scripts_Icons\Line comps.csv"
 
 ForEach($Comp in $Comps){
 
-    xcopy $shortcut "\\$Comp\C$\Users\Public\Desktop\"
+    xcopy "\\usnjfs001\shared\Ryan Curran\Scripts_Icons\medline icons\Machine Breakdown Log.website" "\\$Comp\C$\Users\Public\Desktop\"
 }
 
 pause
