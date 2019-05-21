6dec27822737e635e634c2e7fa5738c8e282ed3e
NightWolf92 <rcurran92@gmail.com>
2019-05-21 19:34:33 -0400

    Delete Terminate employee - Update Convert

--- Convert-UserDisplayNamesToSamAccount.ps1
+++ Convert-UserDisplayNamesToSamAccount.ps1
@@ -1,20 +1,21 @@
-﻿$users = Import-CSV C:\CSV\AplicareEmail.CSV  
+﻿#This is for when you get a csv with displaynames vs the direct samaccountname.
+#It will search the user based on the displayname and spit out the Samaccountname which can be used as an identity for Add-AdGroupmember
+
+$users = Import-CSV C:\CSV\AplicareEmail.CSV 
+
+$adGroup = read-host "Enter AD Group name"
 
 ForEach ($user in $users) {
 
-$dn = $user.DisplayName
+    $samacc = Get-Aduser -Filter { displayName -like $($user.displayname)} | select samaccountname #| export-csv C:\CSV\AplicareEmailSamAccountName.csv -append -NoTypeInformation
 
-    Get-Aduser -Filter { displayName -like $dn} | select samaccountname | export-csv C:\CSV\AplicareEmailSamAccountName.csv -append -NoTypeInformation
-
+        ForEach ($samaccs in $samacc) {
+            Write-host "$($Samaccs.name) - $($Samaccs.Samaccountname) has been added to $adGroup"
+            Add-ADGroupMember -Identity $adGroup -Members $($Samaccs.Samaccountname)
+            
+        } 
+        
 
 }
 
-$samacc = Import-CSV C:\CSV\AplicareEmailSamAccountName.csv
 
-ForEach ($samaccs in $samacc) {
-
-$san = $samaccs.SamAccountName
-
-    Add-ADGroupMember -Identity evprepenable -Members $_.SamAccountName
-
-} 
