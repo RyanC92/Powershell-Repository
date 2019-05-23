b6327c0c4a7505524ff2137e9dae7b0c63e98da0
BuildTools <unconfigured@null.spigotmc.org>
2019-05-23 00:40:13 -0500

    Create Report-Asset_To_User_v5.ps1

---
+++ Report-Asset_To_User_v5.ps1
@@ -0,0 +1,40 @@
+﻿$Node = Import-csv C:\CSV\Test.csv
+
+$Collection = ForEach ($nodes in $node) {
+
+Write-host "Attempting to lookup" $Nodes.Hostname
+
+    try {
+
+        $pt = Test-Connection $($Nodes.Hostname) -Quiet -Count 1 
+
+        if ($open -eq $false) {error}
+
+        $UName = Get-Wmiobject -Computername $Nodes.Hostname -Class Win32_ComputerSystem -ErrorAction Stop | Select-Object UserName
+        $HN = Get-Wmiobject -Computername $Nodes.Hostname -Class Win32_ComputerSystem -ErrorAction Stop | Select-Object PSComputerName
+        $OS = Get-Wmiobject -Computername $Nodes.Hostname -Class Win32_OperatingSystem -ErrorAction Stop | Select-Object Caption
+
+        $properties = @{
+            "UserName" = $($UName.Username)
+            "Hostname" = $($HN.PSComputerName)
+            "OS" = $($OS.Caption)
+
+        }
+
+        New-Object -Typename PSCustomObject -Property $properties
+    }
+
+		Catch [System.Management.Automation.CommandNotFoundException] {
+
+        Write-host "$($nodes.hostname) has failed"
+        "$open"
+
+        $($nodes.Hostname) | Add-Content C:\CSV\Error.log
+	}
+}
+
+$collection | Export-csv C:\CSV\UserInfo$([DateTime]::Now.ToString("MM-dd-yyyy-hh.mm.ss")).csv -NoTypeInformation -Append
+
+Get-Content C:\CSV\Error.log | Add-Content C:\CSV\Error$([DateTime]::Now.ToString("MM-dd-yyyy-hh.mm.ss")).csv 
+
+Remove-Item C:\CSV\Error.log
