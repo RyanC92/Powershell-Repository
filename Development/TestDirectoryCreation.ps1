caaab8f69949783f71ab0dd5890c80bcec29fbfe
NightWolf92 <rcurran92@gmail.com>
2019-10-03 10:08:17 -0400

    Update TestDirectoryCreation.ps1

--- Development/TestDirectoryCreation.ps1
+++ Development/TestDirectoryCreation.ps1
@@ -1,13 +1,22 @@
 $udrivelist = Get-Childitem -Path "\\usnjfs001\H$" -exclude _archive,Batch,Kioware$
 
+#Establish count variable
+$i = 0
+
+#establish parameters for cimsession
+$Computername = 'usnjfs001'
+$fullaccess = 'everyone'
+$Session = New-CimSession -Computername $Computername
+
 ForEach($UDL in $Udrivelist){
 
     $ACL = Get-NTFSaccess -Path "\\USNJFS001\H$\$($UDL.name)" 
 
-    #New-Item -Path "\\usnjfs001\H$\_Archive\Test\$($UDL.Name)" -ItemType Directory
+    New-Item -Path "\\usnjfs001\H$\_Archive\Test\$($UDL.Name)" -ItemType Directory
     
     ForEach($ACLUser in $ACL){
         #ACLUser.Access and ACLUser.PSChildname
+        Write-host "Adding $($ACLUser.AccessRights) for $ACLUser to "
         $ACLUser
         #Set-Acl "C:\Testing\$($UDL.Name)" -AclObject $ACL
 
