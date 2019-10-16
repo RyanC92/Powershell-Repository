a0811f743b1703837860517910584cd346deaa28
NightWolf92 <rcurran92@gmail.com>
2019-10-16 15:35:59 -0400

    Create Report-Get-ADGroup - Excelsior Fileshare Groups.ps1

---
+++ Report-Get-ADGroup - Excelsior Fileshare Groups.ps1
@@ -0,0 +1,19 @@
+﻿Import-Module ActiveDirectory
+
+Set-location AD:
+
+while($local.path -ne "AD:\"){
+
+cd ..
+
+$local = Get-Location
+
+}
+
+Set-location "OU=FileShares,OU=Security,OU=Groups,OU=Special,DC=medline,DC=com"
+
+cls
+
+Get-ADgroup -Filter * | Where{ $_.Name -like "SD-FS-USNJFS001*" -or $_.Name -like "SD-FS-NEP*"}  | select Name, DistinguishedName, GroupCategory, GroupScope |  Export-CSV "C:\CSV\Excelsior Fileshare Groups - Exported $([DateTime]::Now.ToString("MM-dd-yyyy-hh.mm.ss")).csv" -NoTypeInformation
+    cls
+    Write-Host "Report Has Been Created. It is named Excelsior Role Groups - Exported $([DateTime]::Now.ToSTring("MM-dd-yyyy-hh.mm.ss")).csv"
