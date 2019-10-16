6caef4f2aad95abe001911c499ad66b998b1ff30
NightWolf92 <rcurran92@gmail.com>
2019-10-16 15:40:17 -0400

    Update Report-Get-ADGroup - Excelsior Fileshare Groups.ps1

--- Report-Get-ADGroup - Excelsior Fileshare Groups.ps1
+++ Report-Get-ADGroup - Excelsior Fileshare Groups.ps1
@@ -16,4 +16,4 @@ cls
 
 Get-ADgroup -Filter * | Where{ $_.Name -like "SD-FS-USNJFS001*" -or $_.Name -like "SD-FS-NEP*"}  | select Name, DistinguishedName, GroupCategory, GroupScope |  Export-CSV "C:\CSV\Excelsior Fileshare Groups - Exported $([DateTime]::Now.ToString("MM-dd-yyyy-hh.mm.ss")).csv" -NoTypeInformation
     cls
-    Write-Host "Report Has Been Created. It is named Excelsior Role Groups - Exported $([DateTime]::Now.ToSTring("MM-dd-yyyy-hh.mm.ss")).csv"
+    Write-Host "Report Has Been Created. It is named Excelsior Fileshare Groups - Exported $([DateTime]::Now.ToSTring("MM-dd-yyyy-hh.mm.ss")).csv"
