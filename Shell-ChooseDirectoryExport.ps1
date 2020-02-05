0a5d9c96776c9a5e9bfb0c4f50ffd9db8e54b70c
NightWolf92 <rcurran92@gmail.com>
2020-02-05 11:37:41 -0500

    Shell for Creating directory export

---
+++ Shell-ChooseDirectoryExport.ps1
@@ -0,0 +1,12 @@
+  
+#Directory selection for report export
+Function Get-FolderName($InitialDirectory)
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+
+  $OpenFolderDialog = New-Object System.Windows.Forms.FolderBrowserDialog
+  #$OpenFolderDialog.initialDirectory = $initialDirectory
+  #$OpenFileDialog.filter = "CSV (*.csv) | *.csv"
+  $OpenFolderDialog.ShowDialog() | Out-Null
+  $OpenFolderDialog.SelectedPath
+}
