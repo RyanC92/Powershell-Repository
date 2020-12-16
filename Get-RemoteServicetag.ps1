1e23e9f3aa34e1a29ddfadb0ebfbebe62ae17f41
NightWolf92 <rcurran92@gmail.com>
2020-12-16 08:59:03 -0500

    Work in progress for pulling Service tags

---
+++ Get-RemoteServicetag.ps1
@@ -0,0 +1,4 @@
+#Script for pulling service tags from computers
+
+Write-host "Please enter the IP address of the computer" -ForegroundColor Green
+$IP = Read-host "IP"
---
+++ Get-RemoteServicetag_CSV.ps1
@@ -0,0 +1,50 @@
+#Script for pulling service tags from computers
+
+Function Get-FileName($InitialDirectory)
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+
+  $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+  $OpenFileDialog.Dereferencelinks = $False
+  $OpenFileDialog.initialDirectory = $initialDirectory
+  #$OpenFileDialog.filter = "CSV (*.csv) | *.csv"
+  $OpenFileDialog.ShowDialog() | Out-Null
+  $OpenFileDialog.FileName
+}
+
+$i = 0
+
+Write-host "Select the List of Computers to Pull Data From" -ForegroundColor Green
+#Use Function to Get the CSV list of Computers
+$Comps = Get-FileName
+
+$PCS = Import-csv $Comps
+
+ForEach($PC in $PCS){
+
+    $i++ 
+    
+    #Show progress of the count
+    Write-Progress -Activity "Pulling Data from List" -Status "Testing Ping on: $i of $($PCs.count)"
+    Write-Host "Testing $($PC.'Computer Name')"
+   
+    $TC = Test-connection $($PC.'IPv4 Addresses') -Quiet -Count 1
+
+
+    if ($TC -eq $True){
+
+        Write-Host "Connecting to $($PC.'Computer Name') and Pulling Serial Number" -ForegroundColor Green
+        psexec -nobanner \\172.16.155.119 -u tcco\rcurran -p Varkeybl92! powershell 
+        $SN = wmic bios get serialnumber
+        $HN = Hostname
+        $SN | Select @{Name = "Serial Number";Expression = {${$SN.SerialNumber}}}, @{Name = "HostName";Expression = {$HN}} | Set-Content \\SOMLAP0107\C$\Temp\Export.csv
+        
+        exit
+
+        }else{
+
+        Write-Host "$($PC.'Computer Name') Failed the Ping Test" -ForegroundColor Red
+        $PC | Select @{Name = "Computer Name"; Expression = {$($PC.'Computer Name')}}, @{Name = "Last user"; Expression = {$($PC.'Last User')}}, @{Name = "Last Active"; Expression = {$($PC.'Last Active')}} | Export-csv C:\Temp\FailedHostnames.csv -Append -NoTypeinformation
+        }
+
+}
