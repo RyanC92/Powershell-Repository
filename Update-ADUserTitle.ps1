db4972f570d839ad5d36560b58cc2fd7050d076f
NightWolf92 <rcurran92@gmail.com>
2021-08-31 09:41:26 -0400

    Update Update-ADUserTitle.ps1

--- Update-ADUserTitle.ps1
+++ Update-ADUserTitle.ps1
@@ -1,11 +1,19 @@
 #This script would grab each user from an OU Searchbase and check the Title against the description, if the Title is different (which is correct)
 #It will take the Title and replace the description, this is for BU's that use the Description field as a replacement for Title.
 $OU = Read-Host "Please enter the OU that you want to work on" 
-Write-host "You Selected OU - $OU" -ForegroundColor Green -BackgroundColor Yellow
-$Users = Get-aduser -Searchbase "$OU" -properties Title, Description | Select Name, Userprincipalname, Description, Title
+Write-host "You Selected OU - $OU" -ForegroundColor yellow -BackgroundColor green
+$Users = Get-aduser -filter * -Searchbase "$OU" -properties Title, Description | Select Name, Samaccountname, Description, Title
+
 
 Foreach ($User in $Users){
-    Read-host ""
-    Set-aduser -identity "$($User.Userprincipalname)" -Title "$($User.Title)"
-    
+
+    if($($User.Description) -ne ($User.Title)){
+
+        Write-Host "Updating $($User.Name) from $($User.Description) to $($User.Title)" -ForegroundColor Green
+        Set-aduser -identity "$($User.SamAccountName)" -Title "$($User.Title)" -whatif
+
+    }else {
+
+        Write-Host "$($User.Name)'s description ($($User.Description)) is accurate to their title ($($User.Title)), No changes have been made" -ForegroundColor Yellow
+    }
 }
