92db1571ea76c53ab31180a52d5df14f57dd401b
Ryanc92 <rcurran92@gmail.com>
2022-10-24 16:14:10 -0400

    Update Update-ADUserTitle.ps1

--- Update-ADUserTitle.ps1
+++ Update-ADUserTitle.ps1
@@ -1,5 +1,6 @@
 #This script would grab each user from an OU Searchbase and check the Title against the description, if the Title is different (which is correct)
 #It will take the Title and replace the description, this is for BU's that use the Description field as a replacement for Title.
+Import-module activedirectory
 $OU = Read-Host "Please enter the OU that you want to work on" 
 Write-host "You Selected OU - $OU" -ForegroundColor yellow -BackgroundColor Darkgreen
 $Users = Get-aduser -filter * -Searchbase "$OU" -properties Title, Description | Select Name, Samaccountname, Description, Title
