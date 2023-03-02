dc9c3c5d1ca45109b0b34edb18336f601212ab16
Ryan <rcurran92@gmail.com>
2023-03-02 09:39:50 -0500

    Add new fixes

--- TurnerGUI.ps1
+++ TurnerGUI.ps1
@@ -4,85 +4,156 @@ Add-Type -AssemblyName System.Windows.Forms
 # Create the form and its controls
 $form = New-Object System.Windows.Forms.Form
 $textBox = New-Object System.Windows.Forms.TextBox
+$button1 = New-Object System.Windows.Forms.Button
 $button2 = New-Object System.Windows.Forms.Button
 $button3 = New-Object System.Windows.Forms.Button
-$button4 = New-Object System.Windows.Forms.Button
 $panel = New-Object System.Windows.Forms.Panel
 
 
 # Configure the form
 $form.Text = "Turner Fixes"
-$form.Size = New-Object System.Drawing.Size(935, 375)
+$form.Size = New-Object System.Drawing.Size(355, 150)
 $form.StartPosition = "CenterScreen"
 $form.AutoScale = $true
 
-# Configure the text box
-$textBox.Location = New-Object System.Drawing.Size(10, 50)
-$textBox.Size = New-Object System.Drawing.Size(200, 75)
-
 # Configure the buttons
-$button2.Location = New-Object System.Drawing.Size(10, 10)
-$button2.Size = New-Object System.Drawing.Size(100, 25)
-$button2.Text = "Apply SAP Connection Fix"
-$button3.Location = New-Object System.Drawing.Size(120, 10)
-$button3.Size = New-Object System.Drawing.Size(100, 25)
-$button3.Text = "Apply Office Macro Fix"
-$button4.Location = New-Object System.Drawing.Size(230,10)
-$button4.Size = New-Object System.Drawing.Size(100,25)
-$button4.Text = "Apply All Fixes"
-$richtextBox.Location = New-Object System.Drawing.Point (10,75)
-$richtextBox.Size = New-Object System.Drawing.Size(900,250)
-$richtextBox.AutoSize = $True
-
+$button1.Location = New-Object System.Drawing.Size(10, 10)
+$button1.Size = New-Object System.Drawing.Size(100, 40)
+$button1.Text = "Apply SAP Connection Fix"
+$button2.Location = New-Object System.Drawing.Size(120, 10)
+$button2.Size = New-Object System.Drawing.Size(100, 40)
+$button2.Text = "Apply Office Macro Fix"
+$button3.Location = New-Object System.Drawing.Size(230,10)
+$button3.Size = New-Object System.Drawing.Size(100,40)
+$button3.Text = "Apply All Fixes"
+#$richtextBox.Location = New-Object System.Drawing.Point (10,75)
+#$richtextBox.Size = New-Object System.Drawing.Size(900,250)
+#$richtextBox.AutoSize = $True
+$label = New-Object System.Windows.Forms.Label
+$label.autosize = $false
+$label.TextAlign = [System.Drawing.ContentAlignment]::BottomCenter
+$label.Dock = [System.Windows.Forms.DockStyle]::Fill
+#$label.Size = New-Object System.Drawing.Size(250,45)
+#$label.Location = New-Object System.Drawing.Point(75,60)
 
 
 # Add the controls to the form
 
+$form.Controls.Add($button1)
 $form.Controls.Add($button2)
 $form.Controls.Add($button3)
-$form.Controls.Add($button4)
+$form.Controls.Add($label)
 
 # Define the event handlers for the buttons
 $button1_Click = {
-    # Apply the text from the text box to a variable
-    $ethAddress = $textBox.Text
-    Start-process -Verb RunAs -WindowStyle Hidden powershell.exe "New-ItemProperty -Path 'HKLM:\SOFTWARE\Video Miner\InstalledProducts\Video Miner Pool' -Name 'EthereumAddress' -Value $ethAddress -Force"
-    #System.Windows.MessageBox::Show("Ethereum Address has been set to $ethAddress, Restarting the videoMinerSvc")
-    Start-process -Verb RunAs -WindowStyle Hidden powershell.exe "Restart-service -Name videoMinerSvc"
+    Write-Host "Apply SAP Fix"
+    #Apply SAP Connection Fix
+    xcopy "\\tcco.org\tccofile1\TSE_Downloads\SAP Connections Fix\SAP Connections Fix\SAPUILandscapes\*" C:\SAP /y
+    $RegistryPath1  = 'HKLM:\SYSTEM\ControlSet001\Control\Session Manager\Environment'
+    $exists = Test-Path -Path $RegistryPath1
+    if (!$exists) {$null = New-item -Path $RegistryPath1 -Force}
+
+    #Write the registry values
+    'Name, Value, Type
+    PLMVIS_82_LIBPATH,"C:\\Program Files (x86)\\SAP\\FrontEnd\\SAPgui\\Program",REG_SZ
+    SAPLOGON_INI_FILE,"C:\\SAP\\SAPLOGON.INI",REG_SZ
+    SAPLOGON_LSXML_FILE,"C:\\SAP\\SAPUILANDSCAPE.XML", REG_SZ
+    SNC_LIB,"C:\\SAP\\gsskrb5.dll",REG_SZ
+    SNC_LIB_32,"C:\\SAP\\gsskrb5.dll",REG_SZ
+    SNC_LIB_64,"C:\\SAP\\gx64krb5.dll",REG_SZ' |
+    ConvertFrom-Csv |
+    Set-ItemProperty -Path $RegistryPath1 -Name {$_.Name}
+
+    $label.Text = "SAP Fix Applied, You may close the window."
 
 }
 
 $button2_Click = {
-    # Perform some action
-    Write-Host "Start Service"
-    Start-process -Verb RunAs -WindowStyle Hidden powershell.exe "Start-service -Name videoMinerSvc"
+    # Apply Office Macro Fix
+    Write-Host "Apply Office Macro Fix"
+    $RegistryPath1 = 'REGISTRY::HKEY_CLASSES_ROOT\WOW6432Node\CLSID\{5B076C03-2F26-11CF-9AE5-0800096E19F4}'
+    $exists = Test-Path -Path $RegistryPath1
+    if (!$exists) {$null = New-item -Path $RegistryPath1 -Force}
+    'Name, Value, Type
+    @,"SAP Remote Function Call", REG_SZ
+    AppID, {5B076C03-2F26-11CF-9AE5-0800096E19F4}, REG_SZ' |
+    ConvertFrom-Csv |
+    Set-itemProperty -Path $RegistryPath1 -Name {$_.Name}
+
+    $RegistryPath2 = 'REGISTRY::HKEY_CLASSES_ROOT\WOW6432Node\AppID\{5B076C03-2F26-11CF-9AE5-0800096E19F4}'
+    $exists = Test-Path -Path $RegistryPath2
+    if (!$exists) {$null = New-item -Path $RegistryPath2 -Force}
+    'Name, Value, Type
+    DllSurrogate,"",REG_SZ'|
+    ConvertFrom-Csv |
+    Set-itemProperty -Path $RegistryPath2 -Name {$_.Name}
+
+    $RegistryPath3 = 'HKLM:\SOFTWARE\Classes\AppID\{5B076C03-2F26-11CF-9AE5-0800096E19F4}'
+    $exists = Test-Path -Path $RegistryPath3
+    if (!$exists) {$null = New-item -Path $RegistryPath3 -Force}
+    'Name, Value, Type
+    DllSurrogate,"",REG_SZ'|
+    ConvertFrom-Csv |
+    Set-itemProperty -Path $RegistryPath3 -Name {$_.Name}
+
+    $label.Text = "Office Macro Fix Applied, You may close the window."
 }
 
 $button3_Click = {
-    # Perform some action
-    Write-Host "Stop Service"
-    Start-process -Verb RunAs -WindowStyle Hidden powershell.exe "Stop-service -Name videoMinerSvc"
+    # Apply all fixes
+    #SAP Connection Fix
+    xcopy "\\tcco.org\tccofile1\TSE_Downloads\SAP Connections Fix\SAP Connections Fix\SAPUILandscapes\*" C:\SAP /y
+    $RegistryPath1  = 'HKLM:\SYSTEM\ControlSet001\Control\Session Manager\Environment'
+    $exists = Test-Path -Path $RegistryPath1
+    if (!$exists) {$null = New-item -Path $RegistryPath1 -Force}
+
+    #Write the registry values
+    'Name, Value, Type
+    PLMVIS_82_LIBPATH,"C:\\Program Files (x86)\\SAP\\FrontEnd\\SAPgui\\Program",REG_SZ
+    SAPLOGON_INI_FILE,"C:\\SAP\\SAPLOGON.INI",REG_SZ
+    SAPLOGON_LSXML_FILE,"C:\\SAP\\SAPUILANDSCAPE.XML", REG_SZ
+    SNC_LIB,"C:\\SAP\\gsskrb5.dll",REG_SZ
+    SNC_LIB_32,"C:\\SAP\\gsskrb5.dll",REG_SZ
+    SNC_LIB_64,"C:\\SAP\\gx64krb5.dll",REG_SZ' |
+    ConvertFrom-Csv |
+    Set-ItemProperty -Path $RegistryPath1 -Name {$_.Name}
+
+    # Office Macro Fix
+
+    Write-Host "Apply Office Macro Fix"
+    $RegistryPath2 = 'REGISTRY::HKEY_CLASSES_ROOT\WOW6432Node\CLSID\{5B076C03-2F26-11CF-9AE5-0800096E19F4}'
+    $exists = Test-Path -Path $RegistryPath2
+    if (!$exists) {$null = New-item -Path $RegistryPath2 -Force}
+    'Name, Value, Type
+    @,"SAP Remote Function Call", REG_SZ
+    AppID, {5B076C03-2F26-11CF-9AE5-0800096E19F4}, REG_SZ' |
+    ConvertFrom-Csv |
+    Set-itemProperty -Path $RegistryPath2 -Name {$_.Name}
+
+    $RegistryPath3 = 'REGISTRY::HKEY_CLASSES_ROOT\WOW6432Node\AppID\{5B076C03-2F26-11CF-9AE5-0800096E19F4}'
+    $exists = Test-Path -Path $RegistryPath3
+    if (!$exists) {$null = New-item -Path $RegistryPath3 -Force}
+    'Name, Value, Type
+    DllSurrogate,"",REG_SZ'|
+    ConvertFrom-Csv |
+    Set-itemProperty -Path $RegistryPath3 -Name {$_.Name}
+
+    $RegistryPath4 = 'HKLM:\SOFTWARE\Classes\AppID\{5B076C03-2F26-11CF-9AE5-0800096E19F4}'
+    $exists = Test-Path -Path $RegistryPath4
+    if (!$exists) {$null = New-item -Path $RegistryPath4 -Force}
+    'Name, Value, Type
+    DllSurrogate,"",REG_SZ'|
+    ConvertFrom-Csv |
+    Set-itemProperty -Path $RegistryPath4 -Name {$_.Name}
+
+    $label.Text = "All Fixes have been Applied, You may close the window."
+
 }
-
-$button4_Click = {
-    # Perform some action
-    Write-Host "Restart Service"
-    Start-process -Verb RunAs -WindowStyle Hidden powershell.exe "Restart-service -Name videoMinerSvc"
-
-}
-
-$richTextBox | Add-Member -MemberType ScriptMethod -Name "RunCommand" -Value {
-    param($command)
-    $exec = [System.Diagnostics.Process]::Start("cmd.exe", "/c $command")
-    $exec.WaitForExit()
-    $richTextBox.AppendText($exec.StandardOutput.ReadToEnd())
-  }
 
 # Assign the event handlers to the buttons
 $button1.Add_Click($button1_Click)
 $button2.Add_Click($button2_Click)
 $button3.Add_Click($button3_Click)
-$button4.Add_Click($button4_click)
 
 # Show the form
 $form.ShowDialog()
