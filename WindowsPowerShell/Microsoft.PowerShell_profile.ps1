2da851834570a43fa6e1bf9964fabfb496fcac1d
Ryan <rcurran92@gmail.com>
2024-04-09 14:39:12 -0400

    Major Update to SAP Macro Fix

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,44 +3,6 @@ import-module activedirectory
 CD C:\Powershell-Repository
 
 
-Function Connect-2016{
-
-$Credential = Get-Credential -Credential pa-rcurran
-
-Write-Output "Getting Exchange Online cmdlets"
-
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange `
-    -ConnectionUri <sharepointpowershellurl> -Authentication Kerberos -Credential $Credentials
-
-Import-PSSession $session
-
-
-}
-
-
-Function Connect-ExOnline{
-
-$Credential = Get-Credential -Credential Rcurran@excelsiormedical.com
-
-Write-Output "Getting Exchange Online cmdlets"
-
-$session = New-PSSession -ConnectionUri https://ps.outlook.com/Powershell `
-    -ConfigurationName Microsoft.Exchange -Credential $Credential `
-    -Authentication Basic -AllowRedirection
-Import-PSSession $session
-
-Connect-MsolService -Credential $Credential
-
-}
-
-Function PWchange{
-
-$User = Read-Host "User Email Address:"
-$Password = Read-Host "Enter New Password"
-
-Set-Msoluserpassword -UserPrincipalName $User -NewPassword $Password -ForceChangePassword $False 
-
-}
 
 function Get-LoggedOnUser
 {
