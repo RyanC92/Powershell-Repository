55491baeeb2624ac35a7ca3af0e573714abcf6a9
Ryan <rcurran92@gmail.com>
2024-07-11 08:44:35 -0400

    Delete USNJDC003.ps1

--- USNJDC003.ps1
+++
@@ -1,4 +0,0 @@
-﻿$RemServer = "USNJDC003.excelsior.local"
-$s = new-pssession -computer $RemServer
-Invoke-command -session $s -Script {Import-Module ActiveDirectory}
-Import-PSSession -session $s -modulE activedirectory -prefix Rem
